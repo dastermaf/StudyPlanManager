d9d886b0f5797e6dcf496c561e3887bca6da7810
dastermaf <116442860+dastermaf@users.noreply.github.com>
2025-09-20 23:03:18 +0500

    login and registration updates

---
+++ public/js/courseData.js
@@ -0,0 +1,11 @@
+export const courseData = {
+    "CAI2500301": { name: "情報基礎数学2", lectures: 15 },
+    "CAI2500601": { name: "情報システム概論", lectures: 15 },
+    "CAI2500701": { name: "ゲームシステム概論", lectures: 15 },
+    "CAI2500901": { name: "応用プログラミング", lectures: 15 },
+    "CAI2502401": { name: "AI概論", lectures: 15 },
+    "CBD2500501": { name: "コミュニケーション2", lectures: 15 },
+    "CBD2501001": { name: "文化を読みとくB", lectures: 15 },
+    "CBD2502001": { name: "基盤英語2", lectures: 15 },
+    "CAI2500001": { name: "メディアリテラシ", lectures: 15 } // Пример, т.к. точных данных нет
+};
