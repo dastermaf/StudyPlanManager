e1838aa975b7447da32f2ac25db7ea0ff4036596
dastermaf <116442860+dastermaf@users.noreply.github.com>
2025-09-21 11:35:34 +0500

    login and registration updates

--- public/js/courseData.js
+++ public/js/courseData.js
@@ -7,5 +7,6 @@ export const courseData = {
     "CBD2500501": { name: "コミュニケーション2", lectures: 15 },
     "CBD2501001": { name: "文化を読みとくB", lectures: 15 },
     "CBD2502001": { name: "基盤英語2", lectures: 15 },
-    "CAI2500001": { name: "メディアリテラシ", lectures: 15 } // Пример, т.к. точных данных нет
+    "CAI2500001": { name: "メディアリテラシ", lectures: 15 } // ID может быть неточным, но для теста подойдет
 };
+
