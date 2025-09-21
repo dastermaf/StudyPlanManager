96dfd8f5d715d503ffa7320746a75ef581d45d14
dastermaf <116442860+dastermaf@users.noreply.github.com>
2025-09-22 01:54:14 +0500

    login fixes

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 const cors = require('cors');
-// const helmet = require('helmet'); // helmet больше не нужен
+// helmet полностью удален
 const cookieParser = require('cookie-parser');
 const { initializeDatabase } = require('./db');
 const apiRoutes = require('./routes/api');
@@ -18,8 +18,6 @@ const port = process.env.PORT || 3000;
 
 console.log("ЛОГ: server.js: Запуск сервера...");
 app.set('trust proxy', 1);
-
-// app.use(helmet()); // ПОЛНОСТЬЮ УДАЛИТЕ ИЛИ ЗАКОММЕНТИРУЙТЕ HELMET
 
 app.use(cors());
 app.use(express.json({ limit: '2mb' }));
